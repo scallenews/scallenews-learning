53b9a8c2f7a6e4d4f7a29a2e35f6000a0d1e22fa
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-05-23 10:32:01 -0300

    atualização pages `pagina inicial`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Nete, eu amo você. Se você me ama, dá uma risadinha!🤭😎git </h1>;
+  return <h1>Nete, eu amo você. Se você me ama, dá uma risadinha!🤭😎</h1>;
 }
 
 export default Home;
