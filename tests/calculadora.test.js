bfd64598719d6f332cda1f8f28c803ad2170b6a6
scallenews <newronaldojacinto@gmail.com>
2026-05-26 15:30:12 +0000

    Ultima Atualização `codespaces normal`

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -2,18 +2,15 @@ const calculadora = require("../models/calculadora.js");
 
 test("somar 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
-  // console.log(resultado);
   expect(resultado).toBe(4);
 });
 
 test("somar 5 + 100 deveria retornar 105", () => {
   const resultado = calculadora.somar(5, 100);
-  // console.log(resultado);
   expect(resultado).toBe(105);
 });
 
 test("somar 'banana' + 100 deveria retornar 'erro'", () => {
   const resultado = calculadora.somar("banana", 100);
-  // console.log(resultado);
   expect(resultado).toBe("erro");
 });
