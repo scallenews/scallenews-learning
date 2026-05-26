59f41fa4c73aa77c68f34a9cc8d0103dde441e69
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-05-26 12:10:49 -0300

    backup pós `codespaces` quebrar

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,19 @@
-test("espero que 1 seja 1", () => {
-  expect(10).toBe(10);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  // console.log(resultado);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  // console.log(resultado);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  // console.log(resultado);
+  expect(resultado).toBe("erro");
 });
