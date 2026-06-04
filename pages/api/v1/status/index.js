a5210ecd80cdd73435a7aa56c693d530c6bea0e9
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-06-04 08:27:38 -0300

    Update file `pages/api/v1/status/index.js` and file `tests/integration/api/v1/status/get.test.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,14 +6,20 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
       },
     },
   });
 }
-
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,10 +3,12 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+  // console.log(responseBody);
+  // expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.14");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
