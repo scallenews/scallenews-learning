1632c0f43b5d0e6285dff04613a9d091a0f23fad
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-06-14 13:16:36 -0300

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,30 +1,38 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true, // 👈 MODIFICADO: GET apenas simula (True para Dry Run)
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false, // 👈 MODIFICADO: POST executa de verdade (False para Dry Run)
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+    const migreatedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
     });
-    return response.status(200).json(migrations); // Adicionado return por boa prática
-  }
 
+    await dbClient.end();
+
+    if (migreatedMigrations.length > 0) {
+      return response.status(201).json(migreatedMigrations);
+    }
+
+    return response.status(200).json(migreatedMigrations);
+  }
   return response.status(405).end();
 }
