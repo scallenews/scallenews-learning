aa811ef1000eb6da77ebb036367b3bf9688f19bb
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-06-30 17:29:19 -0300

    fix: inclui arquivos de migração no build de produção

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/**/*": ["./infra/migrations/**/*"],
-      "/**/*": ["./infra/migrations/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./infra/migrations/**/*"],
+    "/**/*": ["./infra/migrations/**/*"],
   },
 };
 
