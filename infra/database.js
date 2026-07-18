4a44a1f26ecd9841a7205f5a5c1f4c73adc62c98
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-07-18 07:54:55 -0300

    feat: serviceError in database.js

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Error dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
@@ -41,12 +44,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-
-  if (process.env.NODE_ENV === "production") {
-    return {
-      rejectUnauthorized: true,
-    };
-  }
-
-  return false;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
