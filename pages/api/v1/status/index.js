1472e7e8a785290de8a9b47b40713aa180e6bdc0
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-07-18 15:57:34 -0300

    refactor: remove hash de build da versão do postgres no status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -12,7 +12,8 @@ async function getHandler(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValue =
+    databaseVersionResult.rows[0].server_version.split(" ")[0];
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
