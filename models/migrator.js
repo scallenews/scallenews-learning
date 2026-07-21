2f5eed7170724817c7adec9d3e9b438cfffea88e
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-07-21 09:00:17 -0300

    fix: downgrade `node-pg-migrate` to v6 to fix ESM/CJS compatibility

--- models/migrator.js
+++ models/migrator.js
@@ -1,4 +1,4 @@
-import { runner as migrationRunner } from "node-pg-migrate";
+import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database";
 
