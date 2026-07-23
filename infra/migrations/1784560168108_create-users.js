690ee1001b5f31f78689c2d8ed52e7fa414d1cce
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-07-23 19:50:14 -0300

    feat: commit de feature `o que a aula estava ensinando`

--- infra/migrations/1784560168108_create-users.js
+++ infra/migrations/1784560168108_create-users.js
@@ -1,37 +1,33 @@
-export const up = (pgm) => {
+exports.up = (pgm) => {
   pgm.createTable("users", {
     id: {
       type: "uuid",
       primaryKey: true,
       default: pgm.func("gen_random_uuid()"),
     },
-    // For refence GitHub limits usernames to 39 characters.
+    // For reference, GitHub limits usernames to 39 characters.
     username: {
       type: "varchar(30)",
       notNull: true,
       unique: true,
     },
-
     // Why 254 in length? https://stackoverflow.com/a/1199238
     email: {
       type: "varchar(254)",
       notNull: true,
       unique: true,
     },
-
     // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
       type: "varchar(60)",
       notNull: true,
     },
-
     // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
       notNull: true,
       default: pgm.func("timezone('utc', now())"),
     },
-
     updated_at: {
       type: "timestamptz",
       notNull: true,
@@ -39,5 +35,4 @@ export const up = (pgm) => {
     },
   });
 };
-
-export const down = false;
+exports.down = false;
