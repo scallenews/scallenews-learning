f2e03c8c3b0e83db495d5f574c0cd6e83410f461
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-07-24 14:45:16 -0300

    feat: adiciona validação de username e email únicos no model de usuário

--- models/user.js
+++ models/user.js
@@ -1,22 +1,64 @@
 import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
 
-async function create(userImputValues) {
-  const results = await database.query({
-    text: `
-        INSERT INTO 
-          users (username, email, password)
-        VALUES 
-          ($1, $2, $3)
-        RETURNING
-          *
-        ;`,
-    values: [
-      userImputValues.username,
-      userImputValues.email,
-      userImputValues.password,
-    ],
-  });
-  return results.rows[0];
+async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function validateUniqueEmail(email) {
+    const results = await database.query({
+      text: `
+        SELECT email
+        FROM users
+        WHERE LOWER(email) = LOWER($1)
+      `,
+      values: [email],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+        SELECT username
+        FROM users
+        WHERE LOWER(username) = LOWER($1)
+      `,
+      values: [username],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O username informado já está sendo utilizado.",
+        action: "Utilize outro username para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValues) {
+    const results = await database.query({
+      text: `
+        INSERT INTO users (username, email, password)
+        VALUES ($1, $2, $3)
+        RETURNING *
+      `,
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
+    return results.rows[0];
+  }
 }
 
 const user = {
