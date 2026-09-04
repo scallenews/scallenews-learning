d472e91a72963a42364b6cb60f915aba5d2bc182
ronaldo jacinto <newronaldojacinto@gmail.com>
2026-09-04 06:07:27 -0300

    feat: implement session deletion

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -2,9 +2,13 @@ import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import user from "models/user.js";
 import session from "models/session";
+
 const router = createRouter();
+
 router.get(getHandler);
+
 export default router.handler(controller.errorHandlers);
+
 async function getHandler(request, response) {
   const sessionToken = request.cookies.session_id;
   const sessionObject = await session.findOneValidByToken(sessionToken);
